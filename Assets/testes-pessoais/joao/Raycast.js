38b7552f1fee8758f732b11262beed106564a506
Joao Mantovani <jpm.mantovani@hotmail.com>
2016-09-28 20:27:18 -0300

    Raycast script

---
+++ Assets/testes-pessoais/joao/Raycast.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update() {
+     var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+     var hit : RaycastHit;
+     if (Physics.Raycast (ray, hit, 100)) {
+          if(hit.collider.tag == "Object") {
+                 Debug.Log("Object");
+             }
+     }
+ } 
